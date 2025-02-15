d56f91d4fff4f98ba642b433e8dcf599ce0ebd8d
unknown <daniel00802mic@gamil.com>
2025-02-15 14:27:20 -0800

    fix: separated service for http cals, removed any as a type

---
+++ apps/angular/5-crud-application/src/app/data-access/store.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { randText } from '@ngneat/falso';
+import { Todo } from '../model/todo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoStore {
+  public todos = signal<Todo[]>([]);
+
+  constructor(private http: HttpClient) {}
+
+  getAll() {
+    this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((t) => {
+        this.todos.set(t);
+        // console.log(t);
+      });
+  }
+
+  updateOne(todo: Todo) {
+    this.http
+      .put<Todo>(
+        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+        JSON.stringify({
+          id: todo.id,
+          title: randText(),
+          completed: todo.completed,
+          userId: todo.userId,
+        }),
+        {
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        },
+      )
+      .subscribe((todoUpdated: Todo) => {
+        this.todos.update((prevTodos) => {
+          const updatedTodos = [...prevTodos];
+          const index = updatedTodos.findIndex((t) => t.id === todoUpdated.id);
+          if (index !== -1) {
+            updatedTodos[index] = todoUpdated;
+          }
+          return updatedTodos;
+        });
+      });
+  }
+}
